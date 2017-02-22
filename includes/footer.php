b514c6e45a7362f6ca479aa083e2b0e3d4730d64
Barasbi <web_masters_07@mail.ru>
2017-02-22 12:22:24 +0300

    first commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <section class="section section_padding section_db-footer-view">
 	<div class=" container">
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-8">
 				<ul class="db-footer-ul">
 					<li><a href="#">Твой ТереМ</a></li>
 					<li><a href="#">Карта сайта</a></li>
@@ -10,16 +10,16 @@
 					<li><a href="#">Отзывы </a></li>
 					<li><a href="#">Партнеры</a></li>
 				</ul>
-				<p>Copyright © 2010—2016 Твой Терем. All Rights Reserved. Поддержка сайта - SeoDi.ru</p>
+				<p class="db-p-one">Copyright © 2010—2016 Твой Терем. All Rights Reserved. Поддержка сайта - SeoDi.ru</p>
 			</div>
-			<div class="col-md-6">
+			<div class="col-md-4">
 				<div>
 
-					<p>СПБ +7 (812)<span>748-54-48</span><br>
+					<p><span class="db-city">СПБ</span> +7 (812)<span>748-54-48</span><br>
 					<span>+7 921 <span>950-45-30</span></span>
 						</p>
-					<p>E-mail info@tvoy-terem.ru</p>
-					<p>Моб +7 (921)<span>847-55-06</span></p>
+					<p><span class="db-city">E-mail</span> info@tvoy-terem.ru</p>
+					<p><span class="db-city">Моб</span> +7 (921)<span>847-55-06</span></p>
 					
 
 				</div>
