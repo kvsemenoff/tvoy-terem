dcc87bb4beac00ef9461bd680b161d75ee67163f
Berzeg07 <berezg86@gmail.com>
2017-02-22 11:41:20 +0300

    29.14

--- tech.php
+++ tech.php
@@ -1,10 +1,10 @@
 ﻿<?php require_once('includes/header.php'); ?>
 	
-<h1>Кнопки</h1>
+<h1 class="dd-title">Кнопки</h1>
 <div class="dd-button_1__wrap">
 	<a class="button_1" href="#">Подобрать</a>
-	<a class="button_1" href="#">Подобрать</a>
-	<a class="button_1" href="#">Подобрать</a>
+	<a class="button_1 active_buttons" href="#">Подобрать</a>
+	<a class="button_1 button_1__color" href="#">Подобрать</a>
 </div>	
 
 
