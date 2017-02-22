812138d352ca8351e5f62c71be512b7c62d3ce0a
Berzeg07 <berezg86@gmail.com>
2017-02-22 12:39:18 +0300

    29.14

--- tech.php
+++ tech.php
@@ -1,5 +1,5 @@
 ﻿<?php require_once('includes/header.php'); ?>
-	
+
 <h1 class="dd-title">Кнопки</h1>
 <div class="dd-button_1__wrap">
 	<a class="button_1" href="#">Подобрать</a>
@@ -9,13 +9,25 @@
 
 <div class="dd-button_2__wrap">
 	<a class="button_2 button_2__mright" href="#">Подробнее</a>
-	<a class="button_2 button_2__mright" href="#">Подробнее</a>
-	<a class="button_2 button_2__mright" href="#">Подробнее</a>
+	<a class="button_2 active_buttons_2 button_2__mright" href="#">Подробнее</a>
+	<a class="button_2 active_buttons_3 button_2__mright" href="#">Подробнее</a>
+</div>
+
+<div class="dd-button_3__wrap">
+	<a class="button_3 button_2__mright" href="#">Оставить заявку</a>
+	<a class="button_3 button_3_active button_2__mright" href="#">Оставить заявку</a>
+	<a class="button_3 button_3_color button_2__mright" href="#">Оставить заявку</a>
+</div>
+
+<div class="dd-button_4__wrap">
+	<a class="button_4 button_2__mright" href="#">Перезвоните мне</a>
+	<a class="button_4 button_4_active button_2__mright" href="#">Перезвоните мне</a>
+	<a class="button_4 button_4_color button_2__mright" href="#">Перезвоните мне</a>
 </div>
 
 <?php require_once('includes/footer.php'); ?>
 
-	
+
 </body>
 </html>
 
