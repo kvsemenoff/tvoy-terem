fed6b989ce1a918791f9bef8bf466ea89cdc96cb
engine77 <zaur.ka.00@mail.ru>
2017-02-22 12:39:01 +0300

    20

--- includes/new.php
+++ includes/new.php
@@ -3,59 +3,68 @@
 		<h2>Новые проекты</h2>
 		<h4>домов из бруса</h4>
 		<div class="df-newblock">
-			<div class="df-newbox">
-				<div class="dfnewimg"><img src="img/proect1.png" alt="" class="img-responsive">
-					<span class="dfsquere">126 м²</span>
+			<div id="df-owl">
+				<div class="item">
+					<div class="df-newbox">
+						<div class="dfnewimg"><img src="img/proect1.png" alt="" class="img-responsive">
+							<span class="dfsquere">126 м²</span>
+						</div>
+						<div class="dfnewtxtbox">
+							<a href="#" class="df-proect">Проект ТереМ №23</a>
+							<span class="dfprice">1 200 000 <span>c</span></span>
+							<div class="dfcharacteristicts"><span>Размеры дома:</span><span>8x8</span></div>
+							<div class="dfcharacteristicts"><span>Удобства в доме:</span><span>Есть</span></div>
+							<div class="dfcharacteristicts"><span>Особенности:</span><span>Терраса, Балкон</span></div>
+							<a href="#" name="modal" class="button-1 dfbutton-1">Подробнее</a>
+						</div>
+					</div>
 				</div>
-				<div class="dfnewtxtbox">
-					<a href="#" class="df-proect">Проект ТереМ №23</a>
-					<span class="dfprice">1 200 000 <span>c</span></span>
-					<div class="dfcharacteristicts"><span>Размеры дома:</span><span>8x8</span></div>
-					<div class="dfcharacteristicts"><span>Удобства в доме:</span><span>Есть</span></div>
-					<div class="dfcharacteristicts"><span>Особенности:</span><span>Терраса, Балкон</span></div>
-					<a href="#" name="modal" class="button-1 dfbutton-1">Подробнее</a>
-				</div>
+				<div class="item">
+					<div class="df-newbox">
+						<div class="dfnewimg"><img src="img/proect2.png" alt="" class="img-responsive">
+							<span class="dfsquere">185 м²</span>
+						</div>
+						<div class="dfnewtxtbox">
+							<a href="#" class="df-proect">Проект ТереМ №144</a>
+							<span class="dfprice">2 300 000 <span>c</span></span>
+							<div class="dfcharacteristicts"><span>Размеры дома:</span><span>12x8</span></div>
+							<div class="dfcharacteristicts"><span>Удобства в доме:</span><span>Есть</span></div>
+							<div class="dfcharacteristicts"><span>Особенности:</span><span>Котельная</span></div>
+							<a href="#" name="modal" class="button-1 dfbutton-1">Подробнее</a>
+						</div>
+					</div>
+				</div>	
+				<div class="item">
+					<div class="df-newbox">
+						<div class="dfnewimg"><img src="img/proect3.png" alt="" class="img-responsive">
+							<span class="dfsquere">110 м²</span>
+						</div>
+						<div class="dfnewtxtbox">
+							<a href="#" class="df-proect">Проект ТереМ №28</a>
+							<span class="dfprice">1 000 000 <span>c</span></span>
+							<div class="dfcharacteristicts"><span>Размеры дома:</span><span>10x10</span></div>
+							<div class="dfcharacteristicts"><span>Удобства в доме:</span><span>Есть</span></div>
+							<div class="dfcharacteristicts"><span>Особенности:</span><span>Терраса, Балкон</span></div>
+							<a href="#" name="modal" class="button-1 dfbutton-1">Подробнее</a>
+						</div>
+					</div>
+				</div>	
+				<div class="item">	
+					<div class="df-newbox">
+						<div class="dfnewimg"><img src="img/proect4.png" alt="" class="img-responsive">
+							<span class="dfsquere">146 м²</span>
+						</div>
+						<div class="dfnewtxtbox">
+							<a href="#" class="df-proect">Проект ТереМ №19</a>
+							<span class="dfprice">1 600 000 <span>c</span></span>
+							<div class="dfcharacteristicts"><span>Размеры дома:</span><span>12x8</span></div>
+							<div class="dfcharacteristicts"><span>Удобства в доме:</span><span>Есть</span></div>
+							<div class="dfcharacteristicts"><span>Особенности:</span><span>Терраса, Балкон</span></div>
+							<a href="#" name="modal" class="button-1 dfbutton-1">Подробнее</a>
+						</div>
+					</div>
+				</div>	
 			</div>
-			<div class="df-newbox">
-				<div class="dfnewimg"><img src="img/proect2.png" alt="" class="img-responsive">
-					<span class="dfsquere">185 м²</span>
-				</div>
-				<div class="dfnewtxtbox">
-					<a href="#" class="df-proect">Проект ТереМ №144</a>
-					<span class="dfprice">2 300 000 <span>c</span></span>
-					<div class="dfcharacteristicts"><span>Размеры дома:</span><span>12x8</span></div>
-					<div class="dfcharacteristicts"><span>Удобства в доме:</span><span>Есть</span></div>
-					<div class="dfcharacteristicts"><span>Особенности:</span><span>Котельная</span></div>
-					<a href="#" name="modal" class="button-1 dfbutton-1">Подробнее</a>
-				</div>
-			</div>
-			<div class="df-newbox">
-				<div class="dfnewimg"><img src="img/proect3.png" alt="" class="img-responsive">
-					<span class="dfsquere">110 м²</span>
-				</div>
-				<div class="dfnewtxtbox">
-					<a href="#" class="df-proect">Проект ТереМ №28</a>
-					<span class="dfprice">1 000 000 <span>c</span></span>
-					<div class="dfcharacteristicts"><span>Размеры дома:</span><span>10x10</span></div>
-					<div class="dfcharacteristicts"><span>Удобства в доме:</span><span>Есть</span></div>
-					<div class="dfcharacteristicts"><span>Особенности:</span><span>Терраса, Балкон</span></div>
-					<a href="#" name="modal" class="button-1 dfbutton-1">Подробнее</a>
-				</div>
-			</div>
-			<div class="df-newbox">
-				<div class="dfnewimg"><img src="img/proect4.png" alt="" class="img-responsive">
-					<span class="dfsquere">146 м²</span>
-				</div>
-				<div class="dfnewtxtbox">
-					<a href="#" class="df-proect">Проект ТереМ №19</a>
-					<span class="dfprice">1 600 000 <span>c</span></span>
-					<div class="dfcharacteristicts"><span>Размеры дома:</span><span>12x8</span></div>
-					<div class="dfcharacteristicts"><span>Удобства в доме:</span><span>Есть</span></div>
-					<div class="dfcharacteristicts"><span>Особенности:</span><span>Терраса, Балкон</span></div>
-					<a href="#" name="modal" class="button-1 dfbutton-1">Подробнее</a>
-				</div>
-			</div>
-			
 		</div>
 	</div>
 </section>
