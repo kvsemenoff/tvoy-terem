5490cdcce666cf0b7b9b687ad3317c2a8cda3b34
Barasbi <web_masters_07@mail.ru>
2017-02-22 14:32:29 +0300

    first commit

--- includes/footer.php
+++ includes/footer.php
@@ -2,38 +2,33 @@
 	<div class=" container">
 		<div class="row">
 			<div class="col-md-7">
-				<ul class="db-footer-ul">
-					<li><a href="#">Твой ТереМ</a></li>
-					<li><a href="#">Карта сайта</a></li>
-					<li><a href="#">Контакты</a></li>
-					<li><a href="#">Статьи</a></li>
-					<li><a href="#">Отзывы </a></li>
-					<li><a href="#">Партнеры</a></li>
-				</ul>
-				<p class="db-p-one">Copyright © 2010—2016 Твой Терем. All Rights Reserved. Поддержка сайта - SeoDi.ru</p>
+				<div class="db-up">
+					<ul class="db-footer-ul">
+						<li><a href="#">Твой ТереМ</a></li>
+						<li><a href="#">Карта сайта</a></li>
+						<li><a href="#">Контакты</a></li>
+						<li><a href="#">Статьи</a></li>
+						<li><a href="#">Отзывы </a></li>
+						<li><a href="#">Партнеры</a></li>
+					</ul>
+					<p class="db-p-one">Copyright © 2010—2016 Твой Терем. All Rights Reserved. Поддержка сайта - SeoDi.ru</p>
+				</div>
 			</div>
 			<div class="col-md-5">
-				<div>
 
-					<p class="db-p-two db-p-two-float"><span class="db-city2">СПБ</span> +7 (812)<span class="db-number">748-54-48</span><br>
-					<span class="db-span-out">+7 921 <span class="db-number">950-45-30</span></span>
-						</p>
-					<p class="db-p-two db-p-two-float"><span class="db-city">E-mail</span> info@tvoy-terem.ru</p>
+			<div class="db-down">
+					<p class="db-p-two db-p-two-float"><span class="db-city">СПБ</span> +7 (812)<span class="db-number">748-54-48</span><br>
+						<span class="db-span-out">+7 921 <span class="db-number">950-45-30</span></span>
+					</p>
+					<p class="db-p-two db-p-two-float db-p-p"><span class="db-city2">E-mail</span> info@tvoy-terem.ru</p>
 					<div class="clearfix"></div>
-					<p class="db-p-two"><span class="db-city2">Моб</span> +7 (921)<span class="db-number">847-55-06</span></p>
-					
+					<p class="db-p-two db-p-p2"><span class="db-city">Моб</span> +7 (921)<span class="db-number">847-55-06</span></p>
+
+
 
 				</div>
-				<div>
-				
-				</div>
-				<div>
-					
-				</div>
-				
-			</div>
-			<div class="clearfix"></div>
-			
+
+			</div>	
 		</div>
 	</div>
 </section>
