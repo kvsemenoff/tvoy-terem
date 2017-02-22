ada8c8b3451cd6e38c75c65b7d7295fff5e437a0
Barasbi <web_masters_07@mail.ru>
2017-02-22 14:39:32 +0300

    first commit

--- includes/footer.php
+++ includes/footer.php
@@ -15,19 +15,14 @@
 				</div>
 			</div>
 			<div class="col-md-5">
-
-			<div class="db-down">
+				<div class="db-down">
 					<p class="db-p-two db-p-two-float"><span class="db-city">СПБ</span> +7 (812)<span class="db-number">748-54-48</span><br>
 						<span class="db-span-out">+7 921 <span class="db-number">950-45-30</span></span>
 					</p>
 					<p class="db-p-two db-p-two-float db-p-p"><span class="db-city2">E-mail</span> info@tvoy-terem.ru</p>
 					<div class="clearfix"></div>
 					<p class="db-p-two db-p-p2"><span class="db-city">Моб</span> +7 (921)<span class="db-number">847-55-06</span></p>
-
-
-
 				</div>
-
 			</div>	
 		</div>
 	</div>
