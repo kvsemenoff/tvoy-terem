fcfa26b8f677512969e3e1de69e9ca84faab48f5
Berzeg07 <berezg86@gmail.com>
2017-02-22 11:58:31 +0300

    29.14

--- tech.php
+++ tech.php
@@ -7,6 +7,11 @@
 	<a class="button_1 button_1__color" href="#">Подобрать</a>
 </div>	
 
+<div class="dd-button_2__wrap">
+	<a class="button_2 button_2__mright" href="#">Подробнее</a>
+	<a class="button_2 button_2__mright" href="#">Подробнее</a>
+	<a class="button_2 button_2__mright" href="#">Подробнее</a>
+</div>
 
 <?php require_once('includes/footer.php'); ?>
 
