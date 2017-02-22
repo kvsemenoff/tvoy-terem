7799a78b52d2294abe80f4fd15d80866106b431c
Barasbi <web_masters_07@mail.ru>
2017-02-22 11:59:49 +0300

    first commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,34 @@
 <section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+	<div class=" container">
+		<div class="row">
+			<div class="col-md-6">
+				<ul>
+					<li><a href="#">Твой ТереМ</a></li>
+					<li><a href="#">Карта сайта</a></li>
+					<li><a href="#">Контакты</a></li>
+					<li><a href="#">Статьи</a></li>
+					<li><a href="#">Отзывы </a></li>
+					<li><a href="#">Партнеры</a></li>
+				</ul>
+				<p>Copyright © 2010—2016 Твой Терем. All Rights Reserved. Поддержка сайта - SeoDi.ru</p>
+			</div>
+			<div class="col-md-6">
+				<div>
+					<span>СПБ</span>
+					<span>E-mail</span>
+					<span>Моб</span>
+					
+				</div>
+				<div>
+				
+				</div>
+				<div>
+					
+				</div>
+				
+			</div>
+			<div class="clearfix"></div>
+			
 		</div>
 	</div>
-</div>
 </section>
