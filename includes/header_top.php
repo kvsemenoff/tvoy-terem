5f418517f467c85b5dc1d4779f1d3a54ea9e3bd0
Barasbi <betokov93@gmail.com>
2017-02-22 15:39:24 +0300

    header-top

--- includes/header_top.php
+++ includes/header_top.php
@@ -1,7 +1,25 @@
-<section class="section section_padding">
-<div class=" container">
+<section class="section section_padding header-top">
+<div class="container">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-2 col-md-push-5">
+		    <div class="header-logo-wrap">
+		        <img src="img/logo.png" alt="" class="header-top__logo">
+		    </div>
+		</div> 
+		<div class="col-md-6 col-xs-12 col-md-pull-2 header-top__left header-top__center">
+		    <div class="headertop-spb">
+		        <span class="headertop-spb__phone">+7 (812) <span class="headertop-spb__phonespan"> 748-54-48</span></span>
+		        <span class="headertop-spb__phone">+7 (921) <span class="headertop-spb__phonespan"> 950-45-30</span></span>
+		    </div>
+		    <div class="headertop-mobile">
+		        <span class="headertop-spb__phone last-phone">+7 (921) <span class="headertop-spb__phonespan"> 847-55-06</span></span>
+		    </div>
+		</div>
+		<div class="col-md-4 col-xs-12 header-top__center">
+		    <div class="header-topemail">
+		        <a href="#" class="header-topemail__link">info@tvoy-terem.ru</a>
+		    </div>
+		    <a class="button_4 button_2__mright header-top-btn" href="#js-form1" name="js-modal">Перезвоните мне</a>
 		</div>
 	</div>
 </div>
