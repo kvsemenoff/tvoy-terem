d5e4fb52c2f6a66455ea943ea2b8f3b0e6469eb3
Barasbi <web_masters_07@mail.ru>
2017-02-22 14:43:29 +0300

    first commit

--- includes/footer.php
+++ includes/footer.php
@@ -27,3 +27,16 @@
 		</div>
 	</div>
 </section>
+<div class="mask" id="js-mask"></div>
+<div class="js-window" id="js-form1">
+<div class="db-popap">
+<h1 class="dd-title">Перезвоните</h1>
+<p class="db-title">мне</p>
+<img src="img/db-close.png" alt="img" class="db-close">
+		<form action="#" method="post" class="js-submit">
+			<input type="text" placeholder="Введите ваше имя" name="uname" class="input__view input_background1"><br>
+			<input type="text" placeholder="Введите ваш телефон" name="phone" class="input__view input_background2 js-mask"><br>
+			<input type="submit" name="submit" value="Заказать прямо сечас" class="button-1 db-form-button">
+		</form>
+	</div>
+</div>
