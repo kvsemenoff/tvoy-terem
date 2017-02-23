87bcd5e1fbafc1c1e8e0642414d083a0dc6e854c
Barasbi <betokov93@gmail.com>
2017-02-23 16:13:09 +0300

    add 1

--- js/common.js
+++ js/common.js
@@ -8,18 +8,19 @@ $(document).ready(function(){
     var  id = $(this).attr('href'),
     winW = $(window).width(),
     winH = $(window).height();
-    $(id).css("left", winW/2-$(id).width()/2);
+$(id).css("left", winW/2-320/2);
+
     $('body').css({
       "overflow-y":"hidden",
       paddingRight:"17px"
-  });
+    });
     $(id).fadeIn();
     $('#js-mask').fadeIn();
   });
   $('#js-mask, .db-close').on("click", function(){
     $('#js-mask').hide();
     $('.js-window').hide();
-      $('body').removeAttr("style");
+    $('body').removeAttr("style");
 
   });
 
@@ -54,94 +55,94 @@ $(document).ready(function(){
   };
 
   $(".js-mask").mask("+7 (999) - 999 - 99 - 99?");
-    $('.az-select').each(function(){
-        var select = $(this);    
-        var option = select.find('select option');
-        var str = '<div class="az-options">';
-        select.find('option').each(function(){
-            str+= '<div data-val="' +$(this).val() + '">' + $(this).text() + '</div>'
-        });
-        str+= '</div>';
-        select.html(select.html() + str);
-
-        select.find('select').mousedown(function(){
-            return false;
-        });
-        select.mouseup(function(){
-            select.find('select').focus();
-        });
-        select.find('.az-options div[data-val]').click(function(){
-            select.find('select').val($(this).attr('data-val'));
-        });
-        select.find('select').focusout(function(){
-            if(!select.is(':hover')){
-                select.find('.az-options').slideUp(0);
-                select.removeClass('az-select-focus');
-            }
-        });
+  $('.az-select').each(function(){
+    var select = $(this);    
+    var option = select.find('select option');
+    var str = '<div class="az-options">';
+    select.find('option').each(function(){
+      str+= '<div data-val="' +$(this).val() + '">' + $(this).text() + '</div>'
     });
+    str+= '</div>';
+    select.html(select.html() + str);
 
-    $(".az-select").click(function(){
-        $(this).find('.az-options').slideToggle(0);
-        $(this).toggleClass('az-select-focus');
+    select.find('select').mousedown(function(){
+      return false;
     });
+    select.mouseup(function(){
+      select.find('select').focus();
+    });
+    select.find('.az-options div[data-val]').click(function(){
+      select.find('select').val($(this).attr('data-val'));
+    });
+    select.find('select').focusout(function(){
+      if(!select.is(':hover')){
+        select.find('.az-options').slideUp(0);
+        select.removeClass('az-select-focus');
+      }
+    });
+  });
+
+  $(".az-select").click(function(){
+    $(this).find('.az-options').slideToggle(0);
+    $(this).toggleClass('az-select-focus');
+  });
 
 
 
 
-    var owl2 = $("#df-owl");
-    owl2.owlCarousel({
-        loop:true,
-        nav:true, 
-        autoplay:false,
-        smartSpeed:1000,
-        margin:25,
+  var owl2 = $("#df-owl");
+  owl2.owlCarousel({
+    loop:true,
+    nav:true, 
+    autoplay:false,
+    smartSpeed:1000,
+    margin:25,
         center:false,     //если нужны обрезаные края
         navText:['<span class="df-left"></span>','<span class="df-right"></span>'],
         responsive:{
-            0:{
-              items:1
+          0:{
+            items:1
           },
           480:{
-              items:2  
+            items:2  
           },
           990:{
             items:3
-        },      
+          },      
 
-        1248:{
-          items:4
-      }
-  }
-});
+          1248:{
+            items:4
+          }
+        }
+      });
 
 
 
-    var owl3 = $("#dd-owl");
-    owl3.owlCarousel({
-        loop:true,
-        nav:true, 
-        autoplay:false,
-        smartSpeed:1000,
-        margin:0,
+  var owl3 = $("#dd-owl");
+  owl3.owlCarousel({
+    loop:true,
+    nav:true, 
+    autoplay:false,
+    smartSpeed:1000,
+    margin:0,
         center:false,     //если нужны обрезаные края
         navText:['<span class="df-left"></span>','<span class="df-right"></span>'],
         responsive:{
-            0:{
-              items:1
+          0:{
+            items:1
           },
           480:{
-              items:1  
+            items:1  
           },
           990:{
             items:1
-        },      
+          },      
 
-        1248:{
-          items:1
-      }
-  }
-});
+          1248:{
+            items:1
+          }
+        }
+      });
 
   $("#owl-paper").owlCarousel({
     loop:true,
@@ -149,10 +150,10 @@ $(document).ready(function(){
     items:1,
     responsive:{
       770:{
-         items:2
-      }
-    }
-  });
+       items:2
+     }
+   }
+ });
 
 });
 
