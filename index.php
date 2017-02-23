37bb88eb26b82a0fa8c12423babb7a41b544d184
engine77 <zaur.ka.00@mail.ru>
2017-02-23 13:16:11 +0300

    23

--- index.php
+++ index.php
@@ -7,20 +7,16 @@
 
 	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />	
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
-	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
-	<link rel="stylesheet" href="libs/wow/animate.css">
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />	
 	<link rel="shortcut icon" href="favicon.ico" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700&amp;subset=cyrillic" rel="stylesheet">
 	<link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
 	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
 	<script src="libs/owl.carousel/owl.carousel.js"></script>
-	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
-	<script src="libs/wow/wow.min.js"></script>
-	<script>
-    	new WOW().init();
-    </script>	
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>	
 	<script src="js/jquery.maskedinput.min.js"></script>
 	<script src="js/common.js"></script>
 
