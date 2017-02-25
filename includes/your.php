187e592de3869823ee20b12943bba10b6311ca52
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-02-25 06:38:45 +0600

    last changes

--- includes/your.php
+++ includes/your.php
@@ -4,11 +4,11 @@
 		<span class="title__descr">– каркасные дома, дома и бани из бруса под ключ</span> 
 	</div>
 	<div class="mini-blocks clearfix">
-		<a href="#" class="mini-block">
+		<a href="#" class="mini-block mini-block-first">
 			<img src="img/fon2.jpg" alt="">
 			<i class="back"><span>Деревянные бани</span><b>75 проектов</b></i>
 		</a>
-		<a href="#" class="mini-block">
+		<a href="#" class="mini-block mini-block-second"">
 			<img src="img/fon3.jpg" alt="">
 			<i><span>Каркасные дома</span><b>627 проектов</b></i>
 		</a>
